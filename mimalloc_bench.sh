9b237c37bdee3294b80f014391fbc23f70bbf937
anltran <tran.anthony@aol.com>
2024-05-06 18:01:06 +0000

    Combining scripts and parameterizing benchmarks

--- mimalloc_bench.sh
+++ mimalloc_bench.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for emptiness_fraction in {2..10};
+benchmark=$1
+
+for emptiness_fraction in {2..3};
 do
-    echo Running espresso benchmark with emptiness fraction: $emptiness_fraction
+    echo Running $benchmark benchmark with emptiness fraction: $emptiness_fraction
     sed -i "43s/.*/#define EMPTINESS_CLASSES ${emptiness_fraction}/" /home/anthony/mimalloc-bench/extern/hd/src/include/hoard/hoardheap.h
     cd /home/anthony/mimalloc-bench/extern/hd/src
     make
     cd /home/anthony/mimalloc-bench/out/bench
-    ../../bench.sh hd espresso
+    ../../bench.sh hd $benchmark
 done
